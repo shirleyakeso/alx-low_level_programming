95b7b787605e382cdef8096fffc7162ad1489a4b
shirleyakeso <shirleyakeso@gmail.com>
2022-07-06 17:52:40 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,21 +5,18 @@
 void print_alphabet_x10(void)
 {
 
-	char a ='a';
- 	int n = 10;
+	char a = 'a';
+       	int n = 10;
 
-	while (n>=1)
+	while (n >= 1)
 	{
-	
-		while (a <='z')
+		while (a <= 'z')
 		{
-	
 			_putchar(a);
-	
 			++a;
 		}
 		--n;
 		_putchar(10);
-		a ='a';
+		a = 'a';
 	}
 }
