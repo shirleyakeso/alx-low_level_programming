050135b610d805b9a0225b62a17f40434792a4b1
shirleyakeso <shirleyakeso@gmail.com>
2022-07-06 18:42:00 -0700

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * jack_bauer - prints every minute of the day
+ */
+void jack_bauer(void)
+{
+	int hr, min;
+
+	hr = 0;
+	while (hr < 24)
+	{
+		min = 0;
+		while (min < 60)
+		{
+			_putchar((hr / 10) + '0');
+			_putchar((hr % 10) + '0');
+			_putchar(':');
+			_putchar((min / 10) + '0');
+			_putchar((min % 10) + '0');
+			_putchar('\n');
+			min++;
+		}
+hr++;
+}
