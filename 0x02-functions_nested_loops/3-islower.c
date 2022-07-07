1cc51a48e590ca8d1940d68ebfcee37e6f2ba596
shirleyakeso <shirleyakeso@gmail.com>
2022-07-06 18:04:42 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _islower - function
+ *
+ *@c: the character to evaluate
+ *
+ * Return: Get 1 if c is uppercase,
+ * gey 0 otherwise
+ */
+int_islower(int c)
+{
+if (c >= 97 && c <= 122)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
