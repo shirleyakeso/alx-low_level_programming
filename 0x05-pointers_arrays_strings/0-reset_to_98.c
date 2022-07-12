f7521a13c8ae46c75ae0be2b44e5f62f9ee33e6c
shirleyakeso <shirleyakeso@gmail.com>
2022-07-12 11:52:32 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,11 +2,11 @@
 /**
 *  reset_to_98 ->  a function that takes a pointer to an int as para
 *  meter and updates the value it points to to 98.
-*  @n; character
+*  @n: character
 */
 
 void reset_to_98(int *n)
 {
 *n = 98;
-
+Return: 0 is a success
 }
