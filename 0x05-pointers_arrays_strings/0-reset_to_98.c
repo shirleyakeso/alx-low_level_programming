61544f48d6ce3093e6241b16639063b20937cfa7
shirleyakeso <shirleyakeso@gmail.com>
2022-07-12 11:08:38 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,18 +1,21 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
-* main - check the code
-* Return: Always 0.
-*/
+ *
+ *  *  reset_to_98 ->  a function that takes a pointer to an int as para
+ *
+ *   *  meter and updates the value it points to to 98.
+ *
+ *    *  @n; character
+ *
+ *     */
 
-int main(void)
+
+
+void reset_to_98(int *n)
+
 {
-int n;
 
-n = 402;
-printf("n=%d\n", n);
-reset_to_98(&n);
-printf("n=%d\n", n);
-return (0);
+		*n = 98;
+
 }
