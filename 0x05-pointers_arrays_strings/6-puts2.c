f38cfd1ce3b6069fe93c3daeb4b170770d5e3ea2
shirleyakeso <shirleyakeso@gmail.com>
2022-07-12 19:48:59 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+/**
+* puts2 - A function thay prints a strng startimg from the first character
+*
+* @str: string to be checked
+*/
+
+void puts2(char *str)
+{
+	int i, j;
+	
+	i = strlen(str);
+	for (j = 0; j < i; j += 2)
+	{
+		_putchar(str[j]);
+	}
+	_putchar('\n');
+}
