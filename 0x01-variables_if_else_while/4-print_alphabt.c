7ee215fe0870b95003e0b8d9a61bd109807a3646
shirleyakeso <shirleyakeso@gmail.com>
2022-07-14 02:08:15 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+/**
+* main - prints a to z in lower case without q and e
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int a;
+
+for (a = 'a'; a <= 'z'; a++)
+{
+if (a <= 'd' || (a >= 'f' && a < 'q') || a >= 'r')
+putchar(a);
+}
+putchar('\n');
+return (0);
+}
