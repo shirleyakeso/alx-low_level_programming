533d4963b081d973035e45c3039445ab7fa2c736
shirleyakeso <shirleyakeso@gmail.com>
2022-07-14 01:51:56 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* main - prints a to z in lowercase
+*
+* Return: returns 0 if successful
+*/
+int main(void)
+{
+int a;
+
+for (a = 'a'; a <= 'z'; a++)
+putchar(a);
+putchar('\n');
+return (0);
+}
