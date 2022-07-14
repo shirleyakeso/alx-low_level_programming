d00d947d416c6d9e44ae8e6225ccfa48897b2d5e
shirleyakeso <shirleyakeso@gmail.com>
2022-07-14 02:15:58 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - prints 0 to 9 on a new line each
+*
+* Return: returns 0 if successful
+*/
+int main(void)
+{
+int a = 48;
+
+for (a = 48; a < 58; a++)
+{
+putchar(a);
+}
+putchar('\n');
+return (0);
+}
