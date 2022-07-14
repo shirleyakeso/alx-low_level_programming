2e1246d0e678d93c4c1c1c8d29a87f56d8b52fa7
shirleyakeso <shirleyakeso@gmail.com>
2022-07-14 02:25:09 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+* main - prints all hex numbers in lowercase
+*
+* Return: returns 0 if successful
+*/
+int main(void)
+{
+int a;
+
+char ch;
+for (a = 48; a < 58; a++)
+{
+putchar(a);
+}
+for (ch = 'a'; ch <= 'f'; ch++)
+{
+putchar(ch);
+}
+putchar('\n');
+return (0);
+}
