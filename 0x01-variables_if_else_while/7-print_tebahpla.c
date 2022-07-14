d49e34e738afdb1f708b2977042c97c0804166cc
shirleyakeso <shirleyakeso@gmail.com>
2022-07-14 02:20:01 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* main - prints lower and uppercase alphabets
+*
+* Return: returns 0 if successful
+*/
+int main(void)
+{
+int ch;
+
+for (ch = 'z'; ch >= 'a'; ch--)
+putchar(ch);
+putchar('\n');
+return (0);
+}
