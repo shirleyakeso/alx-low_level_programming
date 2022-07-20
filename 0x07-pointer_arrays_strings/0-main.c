49e9fcbbaa5d6146a2321e7d75d8b4c5cffae609
shirleyakeso <shirleyakeso@gmail.com>
2022-07-20 05:54:33 -0700

    pointers

--- 0x07-pointer_arrays_strings/0-main.c
+++ 0x07-pointer_arrays_strings/0-main.c
@@ -4,27 +4,31 @@
 /**
  * simple_print_buffer - prints buffer in hexa
  * @buffer: the address of memory to print
+ *
  * @size: the size of the memory to print
  *
  * Return: Nothing.
  */
+
 void simple_print_buffer(char *buffer, unsigned int size)
 {
-unsigned int i;
-i = 0;
-while (i < size)
-{
-if (i % 10)
-{
-printf(" ");
-}
-if (!(i % 10) && i)
-printf("\n");
-}
-printf("0x%02x", buffer[i]);
-i++;
-}
-printf("\n");
+	unsigned int i;
+
+	i = 0;
+	while (i < size)
+	{
+		if (i % 10)
+		{
+			printf(" ");
+		}
+		if (!(i % 10) && i)
+		{
+			printf("\n");
+		}
+		printf("0x%02x", buffer[i]);
+		i++;
+	}
+	printf("\n");
 }
 
 /**
@@ -32,12 +36,14 @@ printf("\n");
  *
  * Return: Always 0.
  */
+
 int main(void)
 {
-char buffer[98] = {0x00};
-simple_print_buffer(buffer, 98);
-_memset(buffer, 0x01, 95);
-printf("-------------------------------------------------\n");
-simple_print_buffer(buffer, 98);    
-return (0);
+	char buffer[98] = {0x00};
+
+	simple_print_buffer(buffer, 98);
+	_memset(buffer, 0x01, 95);
+	printf("-------------------------------------------------\n");
+	simple_print_buffer(buffer, 98);
+	return (0);
 }
