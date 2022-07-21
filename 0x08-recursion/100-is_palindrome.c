3154d78e49fd6a5492e81a9397e824cf87f4a295
shirleyakeso <shirleyakeso@gmail.com>
2022-07-21 01:03:10 -0700

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,43 +1,23 @@
 #include "main.h"
 
-
-
 /**
-
  * is_palindrome - checks if a word is a palindrome
-
  * @s: word to be checked
-
  * Return: 1 if palindrome
-
- */
-
-
+*/
 
 int is_palindrome(char *s)
-
 {
-
 	char *p;
-
-
-
-/*	if (*s == '\0')
-
-		return*/
-
-	is_palindrome(s + 1);
-
-	p = &(*s);
-
-	if (*p == *s)
-
-	{
-
-		return (1);
-
-	}
-
-	return (0);
-
+/*
+ * if (*s == '\0')
+return
+*/
+is_palindrome(s + 1);
+p = &(*s);
+if (*p == *s)
+{
+	return (1);
+}
+return (0);
 }
