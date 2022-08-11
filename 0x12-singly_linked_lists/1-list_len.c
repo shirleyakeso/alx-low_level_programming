38900a9b33d0c6fce2ef40264a13956dcec03c20
shirleyakeso <shirleyakeso@gmail.com>
2022-08-11 13:55:12 -0700

    1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,22 +1,31 @@
 #include "lists.h"
 
+
+
 /**
- * list_len - the function that return the number of elements
- * in a linked list.
- * @h: the pointer that points to the struct.
- *
- * Return: number of elements
- *
+ * list_len - number of elements in a list.
+ * @h: name of the list
+ * Return: the number of nodes.
  */
 
 size_t list_len(const list_t *h)
+
 {
-	int elem;
-		elem = 0;
-			while (h != '\0')
-				{
-							h = h->next;
-							elem++;
-						}
-				return (elem);
+
+	int count = 0;
+
+
+
+	while (h)
+
+	{
+
+		count++;
+
+		h = h->next;
+
+	}
+
+	return (count);
+
 }
