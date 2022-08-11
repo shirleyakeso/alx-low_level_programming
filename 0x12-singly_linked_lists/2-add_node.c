be9c5b7d4b7a8fa930d9448c887949a152d295be
shirleyakeso <shirleyakeso@gmail.com>
2022-08-11 14:24:20 -0700

    2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,70 +1,57 @@
 #include "lists.h"
 
-
-
 /**
- * _strlen - function that returns the length of a string.
- * @s : s is a character
- * Return: value is i
- */
-
-int _strlen(const char s)
-
-{
-
-	int i = 0;
-
-
-
-	while (s[i] != '\0')
-
-	{
-
-		i++;
-
-	}
-
-	return (i);
-
-}
-
-
-
-/**
- * add_node - add a new node at beginning of a list_t list.
- * @head: head of a list_t list.
- * @str: value to insert into element.
- * Return: the number of nodes.
+ * add_node - add a new node at the beginning of a list
+ * @head: head of the linked list
+ * @str: string in the struct
+ *
+ * Return: the address of the new element
  */
 
 list_t *add_node(list_t **head, const char *str)
 
 {
 
-	list_t *add;
+	list_t *newnode;
+
+	int newelem = 0;
 
 
 
-	add = malloc(sizeof(list_t));
+	newnode = malloc(sizeof(list_t));
 
-	if (add == NULL)
+
+
+	if (newnode == NULL)
+
+	{
 
 		return (NULL);
 
-	add->str = strdup(str);
+	}
 
 
 
-	add->len = _strlen(str);
-
-	add->next = *head;
-
-	*head = add;
+	newnode->str = strdup(str);
 
 
 
-	return (add);
+	while (str[newelem])
+
+	{
+
+		newelem++;
+
+	}
+
+
+
+	newnode->len = newelem;
+
+	newnode->next = *head;
+
+	*head = newnode;
+
+	return (*head);
 
 }
-
-
